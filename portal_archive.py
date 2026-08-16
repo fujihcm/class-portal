897d6507e896670f05b1322799ee106ad33a5998
Yasuhiro Fujiwara <yfuji@hyo-med.ac.jp>
2026-08-16 14:45:00 +0900

    Update portal_archive.py

--- portal_archive.py
+++ portal_archive.py
@@ -66,13 +66,12 @@ def create_student_page(student_title, target_app_path):
             code = f.read()
 
         # カラムを [左：右] = [3：1] の割合で分割
-        # col1, col2 = st.columns([3, 1])
         col1, col2 = st.columns([3, 1], vertical_alignment="center")
         with col1:
             st.write(f"**🧑‍🎓 {student_title} の作品**")
         with col2:
             # 右上にスイッチを配置
-            show_code = st.toggle("💻 ソース")
+            show_code = st.toggle("💻 ソースコード")
 
         st.divider() # 区切り線
 
